6c118d770262e2b751f9615696d524c7da554f55
icepeng <typ0@naver.com>
2022-02-25 10:19:23 +0900

    refactor skill

--- tests/usage/test_blade_simple_setting.py
+++ tests/usage/test_blade_simple_setting.py
@@ -5,7 +5,7 @@ from loasim.card import lostark_default_card_repository
 from loasim.core import Enemy, InternalStat, Stat
 from loasim.Engraving import EngravingManager
 from loasim.core.stat import sub_pdamage_indep
-from loasim.job import blade
+from loasim.job.blade import lostark_blade_skill_repository
 from loasim.setitem import SetItemState, lostark_setitem_repository
 
 base_internal_stat = InternalStat(
@@ -27,7 +27,11 @@ setitem_state = [
 ]
 
 engraving_manager = EngravingManager(
-    ("원한", 3), ("슈퍼 차지", 3), ("기습의 대가", 3), ("아드레날린", 3), ("잔재된 기운", 3)
+    ("원한", 3),
+    ("슈퍼 차지", 3),
+    ("기습의 대가", 3),
+    ("아드레날린", 3),
+    ("잔재된 기운", 3),
 )
 
 card = lostark_default_card_repository.get("남겨진 바람의 절벽 (12)")
@@ -47,7 +51,8 @@ basis_stat = (
 
 logger.info(basis_stat)
 
-SpinCutter = blade.SpinCutter.build_skill(
+SpinCutter = lostark_blade_skill_repository.build(
+    name="스핀 커터",
     level=10,
     additional_stat=basis_stat.add_indep(
         engraving_manager.get_dynamic_modifier(
@@ -57,7 +62,8 @@ SpinCutter = blade.SpinCutter.build_skill(
     ),
 )
 
-SoulAbsorber = blade.SoulAbsorber.build_skill(
+SoulAbsorber = lostark_blade_skill_repository.build(
+    name="소울 앱소버",
     level=11,
     gem=7,
     tripod={
@@ -72,20 +78,8 @@ SoulAbsorber = blade.SoulAbsorber.build_skill(
     ),
 )
 
-VoidStrike = blade.VoidStrike.build_skill(
-    level=11,
-    gem=7,
-    tripod={
-        "암흑 공격": 4,
-    },
-    additional_stat=basis_stat.add_indep(
-        engraving_manager.get_dynamic_modifier(
-            charge=True,
-            burst_grade=3,
-        )
-    ),
-)
-VoidStrikeExplosion = blade.VoidStrikeExplosion.build_skill(
+VoidStrike = lostark_blade_skill_repository.build(
+    name="보이드 스트라이크",
     level=11,
     gem=7,
     tripod={
@@ -100,7 +94,8 @@ VoidStrikeExplosion = blade.VoidStrikeExplosion.build_skill(
     ),
 )
 
-BlitzRush = blade.BlitzRush.build_skill(
+BlitzRush = lostark_blade_skill_repository.build(
+    name="블리츠 러시",
     level=11,
     gem=7,
     tripod={
@@ -116,7 +111,8 @@ BlitzRush = blade.BlitzRush.build_skill(
     ),
 )
 
-MoonlightSonic = blade.MoonlightSonic.build_skill(
+MoonlightSonic = lostark_blade_skill_repository.build(
+    name="문라이트 소닉",
     level=11,
     gem=7,
     tripod={
@@ -132,7 +128,8 @@ MoonlightSonic = blade.MoonlightSonic.build_skill(
     ),
 )
 
-BladeBurst = blade.BladeBurst.build_skill(
+BladeBurst = lostark_blade_skill_repository.build(
+    name="블레이드 버스트",
     level=11,
     gem=7,
     additional_stat=basis_stat.add_indep(
@@ -149,7 +146,6 @@ dealcycle = [
     (SpinCutter, "back"),
     (SoulAbsorber, "back"),
     (VoidStrike, "back"),
-    (VoidStrikeExplosion, "back"),
     (BlitzRush, "back"),
     (BladeBurst, "back"),
     (MoonlightSonic, "back"),
