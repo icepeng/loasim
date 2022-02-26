6cf178ff29c83f4b0280b7593f09bbbdef045a64
icepeng <typ0@naver.com>
2022-02-27 08:09:37 +0900

    hawkeye

--- loasim/job/hawkeye.py
+++ loasim/job/hawkeye.py
@@ -1,214 +1,285 @@
 from loasim.core import SkillSpecification, Stat, Tripod
+from loasim.core.skill import SkillRepository
 
-Snipe = SkillSpecification(
-    name="스나이프",
-    damage_table={
-        12: (5037, 31.22),
-    },
-    tripods=[
-        Tripod(
-            name="약점 포착",
-            stat_list=[
-                Stat(pdamage_indep=100),
-                Stat(pdamage_indep=111),
-                Stat(pdamage_indep=122),
-                Stat(pdamage_indep=133),
-                Stat(pdamage_indep=145),
-            ],
-        ),
-        Tripod(
-            name="손쉬운 먹잇감",
-            stat_list=[
-                Stat(crit=30, crit_damage=100),
-                Stat(crit=30, crit_damage=115),
-                Stat(crit=30, crit_damage=130),
-                Stat(crit=30, crit_damage=145),
-                Stat(crit=30, crit_damage=160),
-            ],
-        ),
-    ],
+lostark_hawkeye_skill_repository = SkillRepository()
+
+lostark_hawkeye_skill_repository.add(
+    SkillSpecification(
+        name="스나이프",
+        type="Holding",
+        damage_table={
+            12: (5037, 31.22),
+        },
+        tripods=[
+            Tripod(
+                name="약점 포착",
+                stat_list=[
+                    Stat(pdamage_indep=100),
+                    Stat(pdamage_indep=111),
+                    Stat(pdamage_indep=122),
+                    Stat(pdamage_indep=133),
+                    Stat(pdamage_indep=145),
+                ],
+            ),
+            Tripod(
+                name="손쉬운 먹잇감",
+                stat_list=[
+                    Stat(crit=30, crit_damage=100),
+                    Stat(crit=30, crit_damage=115),
+                    Stat(crit=30, crit_damage=130),
+                    Stat(crit=30, crit_damage=145),
+                    Stat(crit=30, crit_damage=160),
+                ],
+            ),
+        ],
+    )
 )
 
-SharpShooter = SkillSpecification(
-    name="샤프 슈터",
-    damage_table={
-        12: (888, 5.53),
-    },
-    multiplier=5,
-    tripods=[
-        Tripod(
-            name="급소 타격",
-            stat_list=[
-                Stat(crit=15),
-                Stat(crit=21),
-                Stat(crit=27),
-                Stat(crit=33),
-                Stat(crit=40),
-            ],
-        ),
-        Tripod(
-            name="약점 포착",
-            stat_list=[
-                Stat(pdamage_indep=40),
-                Stat(pdamage_indep=47.2),
-                Stat(pdamage_indep=54.8),
-                Stat(pdamage_indep=62.4),
-                Stat(pdamage_indep=70),
-            ],
-        ),
-        Tripod(
-            name="집중 사격",
-            stat_list=[
-                Stat(crit_damage=50),
-                Stat(crit_damage=62),
-                Stat(crit_damage=74),
-                Stat(crit_damage=87),
-                Stat(crit_damage=100),
-            ],
-        ),
-    ],
+lostark_hawkeye_skill_repository.add(
+    SkillSpecification(
+        name="샤프 슈터",
+        type="Normal",
+        damage_table={
+            12: (888, 5.53),
+        },
+        multiplier=3,
+        tripods=[
+            Tripod(
+                name="급소 타격",
+                stat_list=[
+                    Stat(crit=15),
+                    Stat(crit=21),
+                    Stat(crit=27),
+                    Stat(crit=33),
+                    Stat(crit=40),
+                ],
+            ),
+            Tripod(
+                name="약점 포착",
+                stat_list=[
+                    Stat(pdamage_indep=40),
+                    Stat(pdamage_indep=47.2),
+                    Stat(pdamage_indep=54.8),
+                    Stat(pdamage_indep=62.4),
+                    Stat(pdamage_indep=70),
+                ],
+            ),
+            Tripod(
+                name="집중 사격",
+                stat_list=[
+                    Stat(pdamage_indep=(500 - 300) / 3, crit_damage=50),
+                    Stat(pdamage_indep=(500 - 300) / 3, crit_damage=62),
+                    Stat(pdamage_indep=(500 - 300) / 3, crit_damage=74),
+                    Stat(pdamage_indep=(500 - 300) / 3, crit_damage=87),
+                    Stat(pdamage_indep=(500 - 300) / 3, crit_damage=100),
+                ],
+            ),
+        ],
+    )
 )
 
-ChargingShot = SkillSpecification(
-    name="차징 샷",
-    damage_table={
-        12: (3386, 20.98),
-    },
-    tripods=[
-        Tripod(
-            name="더블 샷",
-            stat_list=[
-                Stat(pdamage_indep=50),
-                Stat(pdamage_indep=57),
-                Stat(pdamage_indep=64),
-                Stat(pdamage_indep=72),
-                Stat(pdamage_indep=80),
-            ],
-        ),
-        Tripod(
-            name="즉발",
-            stat_list=[  # 더블샷, 즉발 같이 사용시 즉발 1렙으로 적용되는 버그있음
-                Stat(pdamage_indep=50),
-                Stat(pdamage_indep=50),
-                Stat(pdamage_indep=50),
-                Stat(pdamage_indep=50),
-                Stat(pdamage_indep=50),
-            ],
-        ),
-        Tripod(
-            name="정조준",
-            stat_list=[
-                Stat(crit=50, pdamage_indep=30),
-                Stat(crit=50, pdamage_indep=37),
-                Stat(crit=50, pdamage_indep=44),
-                Stat(crit=50, pdamage_indep=52),
-                Stat(crit=50, pdamage_indep=60),
-            ],
-        ),
-    ],
+lostark_hawkeye_skill_repository.add(
+    SkillSpecification(
+        name="차징 샷",
+        type="Charge",
+        damage_table={
+            12: (3386, 20.98),
+        },
+        tripods=[
+            Tripod(
+                name="더블 샷",
+                stat_list=[
+                    Stat(pdamage_indep=50),
+                    Stat(pdamage_indep=57),
+                    Stat(pdamage_indep=64),
+                    Stat(pdamage_indep=72),
+                    Stat(pdamage_indep=80),
+                ],
+            ),
+            Tripod(
+                name="즉발",
+                stat_list=[
+                    Stat(pdamage_indep=50),
+                    Stat(pdamage_indep=54),
+                    Stat(pdamage_indep=58),
+                    Stat(pdamage_indep=62),
+                    Stat(pdamage_indep=66.5),
+                ],
+            ),
+            Tripod(
+                name="정조준",
+                stat_list=[
+                    Stat(crit=50, pdamage_indep=30),
+                    Stat(crit=50, pdamage_indep=37),
+                    Stat(crit=50, pdamage_indep=44),
+                    Stat(crit=50, pdamage_indep=52),
+                    Stat(crit=50, pdamage_indep=60),
+                ],
+            ),
+        ],
+    )
 )
 
-ArrowWave = SkillSpecification(
-    name="애로우 해일",
-    damage_table={
-        12: (378, 2.33),
-    },
-    multiplier=4,
-    tripods=[
-        Tripod(
-            name="강화된 화살",
-            stat_list=[
-                Stat(crit=15),
-                Stat(crit=21),
-                Stat(crit=27),
-                Stat(crit=33),
-                Stat(crit=40),
-            ],
-        ),
-        Tripod(
-            name="저속 탄환",  # 웨이브 해일 적용 기준
-            stat_list=[
-                Stat(pdamage_indep=50),
-                Stat(pdamage_indep=57),
-                Stat(pdamage_indep=64),
-                Stat(pdamage_indep=72),
-                Stat(pdamage_indep=80),
-            ],
-        ),
-        Tripod(
-            name="재빠른 발사",
-            stat_list=[
-                Stat(crit_damage=60),
-                Stat(crit_damage=75),
-                Stat(crit_damage=90),
-                Stat(crit_damage=105),
-                Stat(crit_damage=120),
-            ],
-        ),
-        Tripod(
-            name="웨이브 해일",
-            stat_list=[
-                Stat(pdamage_indep=200),
-                Stat(pdamage_indep=215),
-                Stat(pdamage_indep=230),
-                Stat(pdamage_indep=245),
-                Stat(pdamage_indep=260),
-            ],
-        ),
-    ],
+lostark_hawkeye_skill_repository.add(
+    SkillSpecification(
+        name="애로우 해일",
+        type="Normal",
+        damage_table={
+            12: (378, 2.33),
+        },
+        multiplier=4,
+        tripods=[
+            Tripod(
+                name="강화된 화살",
+                stat_list=[
+                    Stat(crit=15),
+                    Stat(crit=21),
+                    Stat(crit=27),
+                    Stat(crit=33),
+                    Stat(crit=40),
+                ],
+            ),
+            Tripod(
+                name="저속 탄환",  # 웨이브 해일 적용 기준
+                stat_list=[
+                    Stat(pdamage_indep=50),
+                    Stat(pdamage_indep=57),
+                    Stat(pdamage_indep=64),
+                    Stat(pdamage_indep=72),
+                    Stat(pdamage_indep=80),
+                ],
+            ),
+            Tripod(
+                name="재빠른 발사",
+                stat_list=[
+                    Stat(crit_damage=60),
+                    Stat(crit_damage=75),
+                    Stat(crit_damage=90),
+                    Stat(crit_damage=105),
+                    Stat(crit_damage=120),
+                ],
+            ),
+            Tripod(
+                name="웨이브 해일",
+                stat_list=[
+                    Stat(pdamage_indep=200),
+                    Stat(pdamage_indep=215),
+                    Stat(pdamage_indep=230),
+                    Stat(pdamage_indep=245),
+                    Stat(pdamage_indep=260),
+                ],
+            ),
+        ],
+    )
 )
 
-AtomicArrow = SkillSpecification(
-    name="아토믹 애로우",
-    damage_table={
-        12: (142, 0.89),
-    },
-    tripods=[],
+lostark_hawkeye_skill_repository.add(
+    SkillSpecification(
+        name="아토믹 애로우",
+        type="Normal",
+        damage_table={
+            10: (142, 0.77697),
+            11: (142, 0.84498),
+            12: (142, 0.88700),
+        },
+        tripods=[
+            Tripod(
+                name="전격 화살",
+                skill_after="아토믹 애로우(전격)",
+            )
+        ],
+        skill_afters=["아토믹 애로우(폭발)"],
+    )
 )
 
-AtomicArrowExplode = SkillSpecification(
-    name="아토믹 애로우(폭발)",
-    damage_table={
-        12: (1053, 6.53),
-    },
-    tripods=[],
+lostark_hawkeye_skill_repository.add(
+    SkillSpecification(
+        name="아토믹 애로우(폭발)",
+        type="Normal",
+        damage_table={
+            10: (1052, 5.71679),
+            11: (1052, 6.21874),
+            12: (1053, 6.52875),
+        },
+        tripods=[
+            Tripod(
+                name="화살촉 강화",
+                stat_list=[
+                    Stat(crit=30),
+                    Stat(crit=37.5),
+                    Stat(crit=45),
+                    Stat(crit=52.5),
+                    Stat(crit=60),
+                ],
+            ),
+            Tripod(
+                name="긴 도화선",
+                stat_list=[
+                    Stat(pdamage_indep=75),
+                    Stat(pdamage_indep=85),
+                    Stat(pdamage_indep=95),
+                    Stat(pdamage_indep=105),
+                    Stat(pdamage_indep=116),
+                ],
+            ),
+        ],
+    )
 )
 
-AtomicArrowElectric = SkillSpecification(
-    name="아토믹 애로우(전격)",
-    damage_table={
-        12: (166, 1.59),
-    },
-    multiplier=3,
-    tripods=[],
+lostark_hawkeye_skill_repository.add(
+    SkillSpecification(
+        name="아토믹 애로우(전격)",
+        type="Normal",
+        damage_table={
+            10: (1052, 5.71679),
+            11: (1052, 6.21874),
+            12: (1053, 6.52875),
+        },
+        multiplier=3,
+        tripods=[
+            Tripod(
+                name="전격 화살",
+                stat_list=[
+                    Stat(pdamage_indep=15.53 - 100),
+                    Stat(pdamage_indep=18.33 - 100),
+                    Stat(pdamage_indep=21.28 - 100),
+                    Stat(pdamage_indep=24.23 - 100),
+                    Stat(pdamage_indep=27.18 - 100),
+                ],
+            ),
+        ],
+    )
 )
 
-ArrowShower = SkillSpecification(
-    name="애로우 샤워",
-    damage_table={
-        10: (413, 2.244),
-    },
-    multiplier=5,
-    tripods=[
-        Tripod(
-            name="지속력 강화",
-            stat_list=[
-                Stat(pdamage_indep=80),
-                Stat(pdamage_indep=90),
-                Stat(pdamage_indep=100),
-                Stat(pdamage_indep=110),
-                Stat(pdamage_indep=120),
-            ],
-        ),
-        Tripod(
-            name="장대비",  # 지속력 강화 적용 기준
-            stat_list=[
-                Stat(pdamage_indep=20 * 10 / 5),
-                Stat(pdamage_indep=23.4 * 10 / 5),
-                Stat(pdamage_indep=26.8 * 10 / 5),
-                Stat(pdamage_indep=30.4 * 10 / 5),
-                Stat(pdamage_indep=34 * 10 / 5),
-            ],
-        ),
-    ],
+lostark_hawkeye_skill_repository.add(
+    SkillSpecification(
+        name="애로우 샤워",
+        type="Area",
+        damage_table={
+            10: (413, 2.244),
+        },
+        multiplier=5,
+        tripods=[
+            Tripod(
+                name="지속력 강화",
+                stat_list=[
+                    Stat(pdamage_indep=80),
+                    Stat(pdamage_indep=90),
+                    Stat(pdamage_indep=100),
+                    Stat(pdamage_indep=110),
+                    Stat(pdamage_indep=120),
+                ],
+            ),
+            Tripod(
+                name="장대비",  # 지속력 강화 적용 기준
+                stat_list=[
+                    Stat(pdamage_indep=20 * 10 / 5),
+                    Stat(pdamage_indep=23.4 * 10 / 5),
+                    Stat(pdamage_indep=26.8 * 10 / 5),
+                    Stat(pdamage_indep=30.4 * 10 / 5),
+                    Stat(pdamage_indep=34 * 10 / 5),
+                ],
+            ),
+        ],
+    )
 )
--- tests/usage/test_hawkeye_simple_setting.py
+++ tests/usage/test_hawkeye_simple_setting.py
@@ -1,195 +1,145 @@
-from loguru import logger
-
-from loasim.calculate import calculate
-from loasim.card import lostark_default_card_repository
+from loasim.calculate import DealCycle, calculate
 from loasim.core import Enemy, InternalStat, Stat
-from loasim.Engraving import EngravingManager
-from loasim.job import hawkeye
-from loasim.setitem import SetItemState, lostark_setitem_repository
+from loasim.core.buff import OnoffBuff
+from loasim.job.hawkeye import lostark_hawkeye_skill_repository
 
-base_internal_stat = InternalStat(
-    weapon_att=31392, stat_main=133748, crit=607, special=56, swift=1756
-)
-spd = base_internal_stat.get_spd() + 10  # 갈망 +10
 
-base_stat = base_internal_stat.get_stat()
-weapon_stat = Stat(pdamage=23.45)  # 무기 품질
+def generate(internal_stat: InternalStat):
+    AtomicArrowSynergy = OnoffBuff(name="피해 증폭", stat=Stat(pdamage_indep=6))
 
-logger.info(base_stat)
-logger.info(weapon_stat)
+    Snipe = lostark_hawkeye_skill_repository.build(
+        name="스나이프",
+        level=12,
+        gem=7,
+        tripod={
+            "약점 포착": 5,
+            "손쉬운 먹잇감": 5,
+        },
+    )
 
-setitem_state = [
-    SetItemState(
-        name="악몽",
-        level_1=2,
-        level_2=0,
+    SharpShooter = lostark_hawkeye_skill_repository.build(
+        name="샤프 슈터",
+        level=12,
+        gem=7,
+        tripod={
+            "급소 타격": 5,
+            "약점 포착": 5,
+            "집중 사격": 5,
+        },
+    )
+
+    ChargingShot = lostark_hawkeye_skill_repository.build(
+        name="차징 샷",
+        level=12,
+        gem=5,
+        tripod={
+            "더블 샷": 4,
+            "즉발": 4,
+        },
+    )
+
+    ArrowWave = lostark_hawkeye_skill_repository.build(
+        name="애로우 해일",
+        level=12,
+        gem=7,
+        tripod={
+            "강화된 화살": 4,
+            "저속 탄환": 5,
+            "웨이브 해일": 5,
+        },
+    )
+
+    AtomicArrow = lostark_hawkeye_skill_repository.build(
+        name="아토믹 애로우",
+        level=12,
+        gem=5,
+    )
+
+    ArrowShower = lostark_hawkeye_skill_repository.build(
+        name="애로우 샤워",
+        level=10,
+        gem=5,
+        tripod={
+            "지속력 강화": 4,
+            "장대비": 4,
+        },
+    )
+
+    state = {
+        "피해 증폭": True,
+    }
+
+    return DealCycle(
+        buff_list=[AtomicArrowSynergy],
+        skill_list=[
+            (AtomicArrow, state, None),
+            (ArrowShower, state, None),
+            (ArrowWave, state, None),
+            (SharpShooter, state, None),
+            (Snipe, state, None),
+            (ChargingShot, state, None),
+            (AtomicArrow, state, None),
+            (ArrowShower, state, None),
+            (ArrowWave, state, None),
+            (AtomicArrow, state, None),
+            (SharpShooter, state, None),
+            (Snipe, state, None),
+            (ChargingShot, state, None),
+            (AtomicArrow, state, None),
+            (ArrowShower, state, None),
+            (ArrowWave, state, None),
+            (SharpShooter, state, None),
+            (AtomicArrow, state, None),
+            (Snipe, state, None),
+            (ChargingShot, state, None),
+            (AtomicArrow, state, None),
+            (ArrowShower, state, None),
+            (ArrowWave, state, None),
+            (SharpShooter, state, None),
+            (Snipe, state, None),
+            (ChargingShot, state, None),
+            (AtomicArrow, state, None),
+            (ArrowShower, state, None),
+            (AtomicArrow, state, None),
+            (ArrowWave, state, None),
+            (Snipe, state, None),
+            (SharpShooter, state, None),
+            (ChargingShot, state, None),
+            (AtomicArrow, state, None),
+            (ArrowShower, state, None),
+            (ArrowWave, state, None),
+            (Snipe, state, None),
+            (SharpShooter, state, None),
+            (ChargingShot, state, None),
+            (AtomicArrow, state, None),
+            (ArrowShower, state, None),
+            (ArrowWave, state, None),
+            (AtomicArrow, state, None),
+            (Snipe, state, None),
+            (SharpShooter, state, None),
+            (ChargingShot, state, None),
+        ],
+        cycle_time=62,
+    )
+
+
+calculate(
+    internal_stat=InternalStat(
+        weapon_att=31392, stat_main=133748, crit=607, special=56, swift=1756
     ),
-    SetItemState(
-        name="지배",
-        level_1=4,
-        level_2=0,
-    ),
-]
-
-engraving_manager = EngravingManager(
-    ("원한", 3), ("예리한 둔기", 3), ("타격의 대가", 3), ("아드레날린", 3), ("정밀 단도", 3)
+    weapon_pdamage=23.45,
+    setitem_state=[
+        ("악몽", 2, 0),
+        ("지배", 4, 0),
+    ],
+    engraving_state=[
+        ("원한", 3),
+        ("예리한 둔기", 3),
+        ("타격의 대가", 3),
+        ("아드레날린", 3),
+        ("정밀 단도", 3),
+    ],
+    card_state=["남겨진 바람의 절벽 (12)"],
+    generator=generate,
+    enemy=Enemy(armor=6000, reduction=23),
 )
-
-card = lostark_default_card_repository.get("남겨진 바람의 절벽 (12)")
-
-atomic_arrow_synergy = Stat(pdamage_indep=6)
-
-basis_stat = (
-    base_stat
-    + weapon_stat
-    + lostark_setitem_repository.get_stat(setitem_state)
-    + engraving_manager.get_static_modifier()
-    + card.stat
-    + atomic_arrow_synergy
-)
-
-logger.info(basis_stat)
-
-Snipe = hawkeye.Snipe.build_skill(
-    level=12,
-    gem=7,
-    tripod={
-        "약점 포착": 5,
-        "손쉬운 먹잇감": 5,
-    },
-    additional_stat=basis_stat,
-)
-
-SharpShooter = hawkeye.SharpShooter.build_skill(
-    level=12,
-    gem=7,
-    tripod={
-        "급소 타격": 5,
-        "약점 포착": 5,
-        "집중 사격": 5,
-    },
-    additional_stat=basis_stat,
-)
-
-ChargingShot = hawkeye.ChargingShot.build_skill(
-    level=12,
-    gem=5,
-    tripod={
-        "더블 샷": 4,
-        "즉발": 4,
-    },
-    additional_stat=basis_stat,
-)
-
-ArrowWave = hawkeye.ArrowWave.build_skill(
-    level=12,
-    gem=7,
-    tripod={
-        "강화된 화살": 4,
-        "저속 탄환": 5,
-        "웨이브 해일": 5,
-    },
-    additional_stat=basis_stat,
-)
-
-AtomicArrow = hawkeye.AtomicArrow.build_skill(
-    level=12,
-    gem=5,
-    additional_stat=basis_stat,
-)
-AtomicArrowExplode = hawkeye.AtomicArrowExplode.build_skill(
-    level=12,
-    gem=5,
-    additional_stat=basis_stat,
-)
-AtomicArrowElectric = hawkeye.AtomicArrowElectric.build_skill(
-    level=12,
-    gem=5,
-    additional_stat=basis_stat,
-)
-
-ArrowShower = hawkeye.ArrowShower.build_skill(
-    level=10,
-    gem=5,
-    tripod={
-        "지속력 강화": 4,
-        "장대비": 4,
-    },
-    additional_stat=basis_stat,
-)
-
-dealcycle = [  # 62s
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (ArrowShower, None),
-    (ArrowWave, None),
-    (SharpShooter, None),
-    (Snipe, None),
-    (ChargingShot, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (ArrowShower, None),
-    (ArrowWave, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (SharpShooter, None),
-    (Snipe, None),
-    (ChargingShot, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (ArrowShower, None),
-    (ArrowWave, None),
-    (SharpShooter, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (Snipe, None),
-    (ChargingShot, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (ArrowShower, None),
-    (ArrowWave, None),
-    (SharpShooter, None),
-    (Snipe, None),
-    (ChargingShot, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (ArrowShower, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (ArrowWave, None),
-    (Snipe, None),
-    (SharpShooter, None),
-    (ChargingShot, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (ArrowShower, None),
-    (ArrowWave, None),
-    (Snipe, None),
-    (SharpShooter, None),
-    (ChargingShot, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (ArrowShower, None),
-    (ArrowWave, None),
-    (AtomicArrow, None),
-    (AtomicArrowExplode, None),
-    (AtomicArrowElectric, None),
-    (Snipe, None),
-    (SharpShooter, None),
-    (ChargingShot, None),
-]
-cycle_time = 62
-
-enemy = Enemy(armor=6000, reduction=23)
-
-calculate(dealcycle, cycle_time, enemy)
