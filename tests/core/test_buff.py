7335a3ff1a26e180bb0b3bba71b808a2022abe01
icepeng <typ0@naver.com>
2022-03-02 18:07:00 +0900

    update tests

--- tests/core/test_buff.py
+++ tests/core/test_buff.py
@@ -11,19 +11,16 @@ from loasim.core import (
     Stat,
     StatBuff,
 )
+from loasim.core.buff import StaticBuff
 
 
 @pytest.mark.parametrize("onoff, expected_pdamage_indep", [(False, 0), (True, 6)])
 def test_onoff_buff(onoff: bool, expected_pdamage_indep: float, test_skill: Skill):
-    buff_manager = BuffManager(
-        buff_list=[
-            OnoffBuff(
-                name="약점 노출",
-                stat=Stat(pdamage_indep=6),
-            )
-        ]
+    buff = OnoffBuff(
+        name="약점 노출",
+        stat=Stat(pdamage_indep=6),
     )
-    result_stat = buff_manager.get_stat(
+    result_stat = buff.get_stat(
         {"약점 노출": BuffState(onoff=onoff)}, test_skill, Stat(), InternalStat()
     )
 
@@ -34,15 +31,11 @@ def test_onoff_buff(onoff: bool, expected_pdamage_indep: float, test_skill: Skil
     "stack, expected_pdamage_indep", [(0, 0), (1, 2), (2, 4), (3, 6)]
 )
 def test_stack_buff(stack: int, expected_pdamage_indep: float, test_skill: Skill):
-    buff_manager = BuffManager(
-        buff_list=[
-            StackBuff(
-                name="진화의 유산",
-                stat_fn=lambda stack: Stat(pdamage_indep=stack * 2),
-            )
-        ]
+    buff = StackBuff(
+        name="진화의 유산",
+        stat_fn=lambda stack: Stat(pdamage_indep=stack * 2),
     )
-    result_stat = buff_manager.get_stat(
+    result_stat = buff.get_stat(
         {"진화의 유산": BuffState(onoff=True, stack=stack)},
         test_skill,
         Stat(),
@@ -56,18 +49,12 @@ def test_stack_buff(stack: int, expected_pdamage_indep: float, test_skill: Skill
 def test_stat_buff(
     moving_speed: float, expected_pdamage_indep: float, test_skill: Skill
 ):
-    buff_manager = BuffManager(
-        buff_list=[
-            StatBuff(
-                name="돌격대장",
-                stat_fn=lambda stat: Stat(
-                    pdamage_indep=min(stat.moving_speed, 40) * 0.45
-                ),
-            )
-        ]
+    buff = StatBuff(
+        name="돌격대장",
+        stat_fn=lambda stat: Stat(pdamage_indep=min(stat.moving_speed, 40) * 0.45),
     )
 
-    result_stat = buff_manager.get_stat(
+    result_stat = buff.get_stat(
         {}, test_skill, Stat(moving_speed=moving_speed), InternalStat()
     )
 
@@ -78,18 +65,14 @@ def test_stat_buff(
 def test_internal_stat_buff(
     special: float, expected_pdamage_indep: float, test_skill: Skill
 ):
-    buff_manager = BuffManager(
-        buff_list=[
-            InternalStatOnoffBuff(
-                name="저무는 달",
-                stat_fn=lambda internal_stat: Stat(
-                    pdamage_indep=10 * (1 + internal_stat.special * 0.000543611)
-                ),
-            )
-        ]
+    buff = InternalStatOnoffBuff(
+        name="저무는 달",
+        stat_fn=lambda internal_stat: Stat(
+            pdamage_indep=10 * (1 + internal_stat.special * 0.000543611)
+        ),
     )
 
-    result_stat = buff_manager.get_stat(
+    result_stat = buff.get_stat(
         {"저무는 달": BuffState(onoff=True)},
         test_skill,
         Stat(),
@@ -97,3 +80,28 @@ def test_internal_stat_buff(
     )
 
     assert abs(result_stat.pdamage_indep - expected_pdamage_indep) < 0.0005
+
+
+@pytest.mark.parametrize(
+    "moving_speed, expected_pdamage_indep", [(0, 15 * 0.45), (20, 35 * 0.45)]
+)
+def test_buff_manager(
+    moving_speed: float, expected_pdamage_indep: float, test_skill: Skill
+):
+    buff_manager = BuffManager(
+        [
+            StatBuff(
+                name="돌격대장",
+                stat_fn=lambda stat: Stat(
+                    pdamage_indep=min(stat.moving_speed, 40) * 0.45
+                ),
+            ),
+            StaticBuff(name="정기 흡수", stat=Stat(moving_speed=15)),
+        ]
+    )
+
+    result_stat = buff_manager.get_stat(
+        {}, test_skill, Stat(moving_speed=moving_speed), InternalStat()
+    )
+
+    assert result_stat.pdamage_indep == expected_pdamage_indep
--- tests/core/test_skill.py
+++ tests/core/test_skill.py
@@ -7,9 +7,116 @@ from loasim.core import Enemy, Skill, Stat
 def test_skill_damage(
     att: float,
     expected_damage: float,
-    test_skill: Skill,
 ):
+    test_skill = Skill(
+        name="테스트 스킬",
+        type="Normal",
+        base=1000,
+        coefficient=10,
+        multiplier=1,
+        head=False,
+        back=False,
+        consume_mana=True,
+        stat=Stat(),
+        skill_afters=[],
+    )
+
     assert (
         test_skill.get_damage(Enemy(armor=0, reduction=0), None, Stat(att=att))
+        == expected_damage
+    )
+
+
+@pytest.mark.parametrize(
+    "att, expected_damage", [(1000, 11000 + 5500), (0, 1000 + 500)]
+)
+def test_chained_skill_damage(
+    att: float,
+    expected_damage: float,
+):
+    trailing_skill = Skill(
+        name="후속 스킬",
+        type="Normal",
+        base=500,
+        coefficient=5,
+        multiplier=1,
+        head=False,
+        back=False,
+        consume_mana=True,
+        stat=Stat(),
+        skill_afters=[],
+    )
+
+    test_chained_skill = Skill(
+        name="테스트 스킬",
+        type="Normal",
+        base=1000,
+        coefficient=10,
+        multiplier=1,
+        head=False,
+        back=False,
+        consume_mana=True,
+        stat=Stat(),
+        skill_afters=[trailing_skill],
+    )
+
+    assert (
+        test_chained_skill.get_damage(Enemy(armor=0, reduction=0), None, Stat(att=att))
+        == expected_damage
+    )
+
+
+@pytest.mark.parametrize("att, expected_damage", [(1000, 11000 * 1.2), (0, 1000 * 1.2)])
+def test_skill_damage_head(
+    att: float,
+    expected_damage: float,
+):
+    test_skill = Skill(
+        name="테스트 스킬",
+        type="Normal",
+        base=1000,
+        coefficient=10,
+        multiplier=1,
+        head=True,
+        back=False,
+        consume_mana=True,
+        stat=Stat(),
+        skill_afters=[],
+    )
+
+    assert (
+        test_skill.get_damage(Enemy(armor=0, reduction=0), "head", Stat(att=att))
+        == expected_damage
+    )
+
+
+@pytest.mark.parametrize(
+    "att, expected_damage",
+    [
+        (1000, (0.9 * 11000 + 0.1 * 11000 * 2) * 1.05),
+        (0, (0.9 * 1000 + 0.1 * 1000 * 2) * 1.05),
+    ],
+)
+def test_skill_damage_back(
+    att: float,
+    expected_damage: float,
+):
+    test_skill = Skill(
+        name="테스트 스킬",
+        type="Normal",
+        base=1000,
+        coefficient=10,
+        multiplier=1,
+        head=False,
+        back=True,
+        consume_mana=True,
+        stat=Stat(),
+        skill_afters=[],
+    )
+
+    assert (
+        test_skill.get_damage(
+            Enemy(armor=0, reduction=0), "back", Stat(att=att, crit_damage=200)
+        )
         == expected_damage
     )
